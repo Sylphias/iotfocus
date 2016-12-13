74908acdb3a8445592c67fa741b30edc0c08c795
Sylphias <sylpharyon@gmail.com>
2016-12-14 05:49:35 +0800

    add precompiled assets

---
+++ public/assets/index_charts-eaa6a629332912a01afe99fc8411d9661209c7cabf7aba11acadb0d7c35a8e30.js
@@ -0,0 +1,73 @@
+
+$( window ).load(function() {
+
+  var data = {
+    labels: gon.day_states_keys,
+    datasets: [
+        {
+            data: gon.day_states_values,
+            backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56",
+                "#16a085",
+                "#ecf0f1",
+                "#d35400"
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56",
+                "#16a085",
+                "#ecf0f1",
+                "#d35400"
+            ]
+        }]
+
+  };
+  initializeIndexCharts();
+});
+function initializeIndexCharts(){
+  var data = {
+    labels: gon.day_states_keys,
+    datasets: [
+        {
+            data: gon.day_states_values,
+            backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56",
+                "#16a085",
+                "#ecf0f1",
+                "#d35400"
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56",
+                "#16a085",
+                "#ecf0f1",
+                "#d35400"
+            ]
+        }]
+
+  };
+  console.log(gon.day_states_keys)
+  console.log(gon.day_states_values)
+    try{
+      var myPieChart = new Chart($(".isc").get(0).getContext("2d"),{
+        type: 'pie',
+        data: data,
+        options: {
+            legend: {
+                display: true,
+            },
+        }
+      });
+  }
+  catch (e){
+    console.log("indiv page" + e)
+
+  }
+}
+;

315f8028194d104e007ec783b3f4c4471fab1671
Sylphias <sylpharyon@gmail.com>
2016-12-13 21:43:12 +0800

    Fix emotion datum error, short of 1 bracket

--- app/models/emotion_datum.rb
+++ app/models/emotion_datum.rb
@@ -1,6 +1,6 @@
 class EmotionDatum < ApplicationRecord
   belongs_to :person
   scope :get_day_emotions, ->{EmotionDatum.select(:feeling).where(created_at: (Date.today-1)..Date.today)}
-  scope :get_5minutes__emotions, -> {EmotionDatum.select(:feeling).where(created_at:(Time.now-(5*60))}
+  scope :get_5minutes__emotions, -> {EmotionDatum.select(:feeling).where(created_at: Time.now-(5*60)..Time.now)}
 
 end

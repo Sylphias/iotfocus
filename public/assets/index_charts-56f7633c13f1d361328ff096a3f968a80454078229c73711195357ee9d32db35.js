d7028415a3fbf80fe7a166412685d8ff2eee5fff
Sylphias <sylpharyon@gmail.com>
2016-12-14 04:19:17 +0800

    Recompiled assets

---
+++ public/assets/index_charts-56f7633c13f1d361328ff096a3f968a80454078229c73711195357ee9d32db35.js
@@ -0,0 +1,20 @@
+
+function initializeIndexCharts(){
+    try{
+      console.log($(".isc").get(0).getContext("2d"));
+      var myPieChart = new Chart($(".isc").get(0).getContext("2d"),{
+        type: 'pie',
+        data: data,
+        options: {
+            legend: {
+                display: true,
+            },
+        }
+      });
+  }
+  catch (e){
+    console.log("indiv page" + e)
+
+  }
+}
+;

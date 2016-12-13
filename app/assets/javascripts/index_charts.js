6c877f5a79498ef65a244fe4f72d4bdf4347d14f
Sylphias <sylpharyon@gmail.com>
2016-12-14 04:04:28 +0800

    fix minor typo

--- app/assets/javascripts/index_charts.js
+++ app/assets/javascripts/index_charts.js
@@ -1,25 +1,19 @@
 
 function initializeIndexCharts(){
-  try{
-    console.log($(".isc").get(0).getContext("2d"));
-    var myPieChart = new Chart($(".isc").get(0).getContext("2d"),{
-      type: 'pie',
-      data: data,
-      options: {
-          legend: {
-              display: true,
-          },
-          
-      }
-    });
-}
-// try{  
-//   var myLineChart = new Chart($(".indiv-chart").get(0).getContext("2d"), {
-//     type: 'line',
-//     data: individual_data,
-//   });
-// }
-catch (e){
-  console.log("indiv page" + e)
+    try{
+      console.log($(".isc").get(0).getContext("2d"));
+      var myPieChart = new Chart($(".isc").get(0).getContext("2d"),{
+        type: 'pie',
+        data: data,
+        options: {
+            legend: {
+                display: true,
+            },
+        }
+      });
+  }
+  catch (e){
+    console.log("indiv page" + e)
 
+  }
 }
